f847c806dcc373ae76d5f0eeb49d6468f39b46bb
Yoandy/PV <yoandypv@gmail.com>
2019-05-16 15:25:37 -0400

    sdk for android v0.1

---
+++ src/main/java/ca/tourism/platform/sdk/Run.java
@@ -0,0 +1,16 @@
+package ca.tourism.platform.sdk ;
+
+import ca.tourism.platform.sdk.entity.country.Country;
+import ca.tourism.platform.sdk.entity.page.CustomPage;
+import ca.tourism.platform.sdk.exceptions.HttpException;
+import ca.tourism.platform.sdk.services.CountryProvider;
+
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class Run {
+
+	public static void main(String[] args) {
+	}
+}
